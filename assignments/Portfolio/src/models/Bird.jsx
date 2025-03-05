395022248e02e74a669a7ac8b785f4936f0a25f5
mjxang <mjxang@gmail.com>
2025-03-05 14:06:22 -0500

    trying to simulate bird flying in a sin wave

--- assignments/Portfolio/src/models/Bird.jsx
+++ assignments/Portfolio/src/models/Bird.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { useGLTF } from '@react-three/drei';
+import React, {useRef, useEffect} from 'react'
+import { useGLTF, useAnimations } from '@react-three/drei'
 
 import birdScene from '../assets/3d/bird.glb';
+import { useFrame } from '@react-three/fiber';
 
-const Bird = () => {
+const Bird = ({isRotating}) => {
     const { scene, animations } = useGLTF(birdScene);
+    const birdRef = useRef()
+    const {actions} = useAnimations(animations, birdRef)
+
+    useEffect (() => {
+actions['Take 001'].play();
+
+    }, []);
+
+    useFrame((_, delta) => {
+        birdRef.current.rotation.y += 0.15 * delta;
   return (
-    <mesh position={[-5,2,1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh 
+    position={[-5,2,1]} 
+    scale={[0.003, 0.003, 0.003]} 
+    ref={birdRef}>
       <primitive object={scene} />
     </mesh>
   )
