af65378eafbcae141212c622485f7777d72bce6b
mjxang <mjxang@gmail.com>
2025-03-04 20:49:25 -0500

    3d timeeeee (still following the youtube video)

--- assignments/Portfolio/src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">About Me</h1>
-    </section>
-  )
-}
-
-export default About
--- assignments/Portfolio/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-    </section>
-  )
-}
-
-export default Home
