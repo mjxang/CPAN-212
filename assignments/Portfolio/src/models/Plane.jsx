d861c394539ebccf50710eaca2b7a3207c4e8ad5
mjxang <mjxang@gmail.com>
2025-03-05 12:59:44 -0500

    added console log for plane and fixed syntax errors

--- assignments/Portfolio/src/models/Plane.jsx
+++ assignments/Portfolio/src/models/Plane.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
+import { useGLTF, useAnimations } from '@react-three/drei'
+import planeScene from '../assets/3d/plane.glb'
 
-import planeScene from '../assets/3d/plane.glb';
-import { useGLTF } from '@react-three/drei';
-
-const Plane = ({isRotating, ...props}) => {
+const Plane = ({ isRotating, ...props }) => {
+    const ref = useRef();
     const { scene, animations } = useGLTF(planeScene);
-  return (
-    <mesh {...props}>
-      <primitive object={scene} />
-    </mesh>
-  )
-}
+    const { actions } = useAnimations(animations, ref);
 
-export default Plane
+    useEffect(() => {
+        console.log({ isRotating });
+        if (isRotating) {
+            actions['Take 001'].play();
+        } else {
+            actions['Take 001'].stop();
+        }
+    }, [actions, isRotating]);
+
+    return (
+        <mesh {...props} ref={ref}>
+            <primitive object={scene} />
+        </mesh>
+    );
+};
+
+export default Plane;
