0723b2689a021e7fb6492c824195952d92b38a93
mjxang <mjxang@gmail.com>
2025-03-05 23:42:42 -0500

    updated my service and template ids and that fixed my issue

--- assignments/Portfolio/src/pages/Contact.jsx
+++ assignments/Portfolio/src/pages/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    console.log();
+    console.log(      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
