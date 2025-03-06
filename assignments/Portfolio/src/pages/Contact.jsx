ca5dd60a1cda2a90edbfcb32830158065f97dc7a
mjxang <mjxang@gmail.com>
2025-03-05 22:54:36 -0500

    setting up emailjs

--- assignments/Portfolio/src/pages/Contact.jsx
+++ assignments/Portfolio/src/pages/Contact.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value }) 
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.sendForm(
+     
+    )
+
+  };
   const handleFocus = () => {};
   const handleBlur = () => {};
+  
   return (
     <section className = "relative flex lg:flex-row flex-col max-container">
 <div className="flex-1 min-w-[50%] flex flex-col">
 <h1 className="head-text">Get in Touch</h1>
 <form 
   className="w-full flex flex-col gap-7 mt-14"
+  onSubmit={handleSubmit}
   >
     <label className="text-black-500 font-semibold">
       Name
