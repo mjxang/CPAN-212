fbbb73b71ee82c814c9bfe178911d31e91d5ed5f
mjxang <mjxang@gmail.com>
2025-03-06 22:40:24 -0500

    starting my projects page

--- assignments/Portfolio/src/pages/Projects.jsx
+++ assignments/Portfolio/src/pages/Projects.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="max-container">
+      <h1 className="head-text">
+        My <span className="blue-gradient_text font-semibold drop-shadow">Projects</span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+        I haven’t done many projects, but the ones I have worked on showcase my passion for problem-solving, creativity, and building functional solutions. Whether its web development, database design, or experimenting with AI, each project has helped me grow and refine my skills. I’m always eager to learn and take on new challenges—feel free to take a look around!</p>
+      </div>
+
+      <div className="flex flex-wrap my-20 gap-16">
+
+        
+      </div>
+      </section>
   )
 }
 
