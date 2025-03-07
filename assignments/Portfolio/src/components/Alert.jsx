eea371a6263e932f10980cf26388a6d764e0d6e3
mjxang <mjxang@gmail.com>
2025-03-06 20:14:21 -0500

    starting my about section

--- assignments/Portfolio/src/components/Alert.jsx
+++ assignments/Portfolio/src/components/Alert.jsx
@@ -9,13 +9,18 @@ const Alert = ({ show, type, text }) => {
     >
       <div
         className={`${
-          type === 'danger' ? 'bg-red-800' : 'bg-green-800'
-        } p-4 text-white rounded-lg font-medium max-w-md mx-4`}
+          type === 'danger' ? 'bg-red-800' : 'bg-blue-800'
+        } p-2 text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex items-center`}
+        role="alert"
       >
-        {text}
+        <p className={`${
+          type === 'danger' ? 'bg-red-500' : 'bg-blue-500'
+        } flex rounded-full uppercase px-2 py-1 font-semibold mr-3 text-xs`}>
+          {type === 'danger' ? 'Failed' : 'Success'}
+        </p>
+        <p className="mr-2 text-left flex-auto">{text}</p>
       </div>
     </div>
   );
 };
-
 export default Alert
--- assignments/Portfolio/src/pages/About.jsx
+++ assignments/Portfolio/src/pages/About.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="max-container">
+<h1 className="head-text">
+  Hello, I'm <span className="blue-gradient_text font-semibold drop-shadow">MJ</span>
+  </h1>
+
+  <div className="mt-5 flex flex-col gap-3 text-slate-500">
+    <p>A Computer Programming Student at Humber College
+       passionate about learning new programming languages and expanding my skillset.
+    </p>
+  </div>
+
+  <div className="py-1- flex flex-col">
+    <h3 className="subhead-text">My skills</h3>
+
+    <div className="mt-16 flex flex-wrap gap-12">
+
+
+    </div>
+  </div>
+  </section>
   )
 }
 
